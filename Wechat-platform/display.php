6d9cd9655c3db4e171cd2a54c381c2dca52972ea
dazzle <654504711@qq.com>
2015-03-03 20:09:31 +0800

    Update display.php

--- Wechat-platform/display.php
+++ Wechat-platform/display.php
@@ -1,18 +1,5 @@
 <?php
-/*
-//测试ACC_TOCKEN的值
-$APPID="wx0184a7e3de922b62";
-$APPSECRET="6000a92ac03958eb91c405de65b0d9e2";
 
-$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=".$APPID."&secret=".$APPSECRET;
-
-$json=file_get_contents($TOKEN_URL);
-$result=json_decode($json,true);
-
-$ACC_TOKEN=$result['access_token'];
-
-echo $ACC_TOKEN;
-*/
 header("Content-type: text/html; charset=utf-8");
 define("ACCESS_TOKEN", '');
 
@@ -110,67 +97,6 @@ $menu = '{
         ]
 }';
 
-/*$menu = '{
-		"button":[
-        {
-        	"name":"关于我们",
-            "sub_button":[
-            {
-            	"name":"小组简介",
-                "type":"click",
-                "key":"group_introduction"
-            },
-            {
-            	"name":"联系我们",
-                "type":"click",
-                "key":"about_us"
-            },
-            {
-            	"name":"使用帮助",
-                "type":"click",
-                "key":"use_help"
-            },
-            {            
-            	"name":"捐助我们",
-                "type":"click",
-                "key":"donate"
-            }
-            
-            ]
-        },
-        {
-        	"name":"Linux学习",
-            "sub_button":[
-            {
-            	"name":"Linux编程",
-                "type":"click",
-                "key":"linux_programmer"
-            },
-            {
-            	"name":"Linux运维",
-                "type":"click",
-                "key":"linux_operation"
-            },
-            {
-            	"name":"算法资料",
-                "type":"click",
-                "key":"data_algorithm"
-            },
-            {
-            	"name":"内核资料",
-                "type":"click",
-                "key":"data_kernel"
-            }
-            ]
-        },
-       
-        {
-        	"name":"活动报名",
-            "type":"click",
-            "key":"softday_register"
-        }
-        ]
-}';*/
 
 
 
